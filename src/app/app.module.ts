c9a844bef77646b561402420fb85927585ecf7ee
VVISHALGOUR0001 <vishalgour0020@gmail.com>
2023-05-29 17:58:35 +0530

    home component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ComponentComponent } from './component/component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ComponentComponent
   ],
   imports: [
     BrowserModule

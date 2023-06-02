d8f272ca4109dadf63007095dc5830e6fb48a30f
VVISHALGOUR0001 <vishalgour0020@gmail.com>
2023-06-02 17:18:39 +0530

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ComponentsComponent } from './components/components.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ComponentsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

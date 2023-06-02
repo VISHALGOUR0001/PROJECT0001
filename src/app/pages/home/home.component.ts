79959f88573a5030ce66bd4ac155c991117893e7
VVISHALGOUR0001 <vishalgour0020@gmail.com>
2023-06-02 17:57:56 +0530

    push2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { identity } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-public data="hello";
 public color="text-primary";
 public con=true;
+public data= [
+   {
+    name:'aaaa',
+    id:1,
+  },
+  {
+    name:'bbbb',
+    id:2,
+  },
+  {
+    name:'cccc',
+    id:3,
+  },
+  {
+    name:'dddd',
+    id:4,
+  },
+  {
+    name:'eeee',
+    id:5,
+  } ,
+  {
+    name:'ffff',
+    id:6,
+  },
+  {
+    name:'gggg',
+    id:7,
+  },
+]
 }

90d5375bf881c9211ddb6bb24c3fa5433612efb9
VVISHALGOUR0001 <vishalgour0020@gmail.com>
2023-06-03 18:58:49 +0530

    about.html/about.css/aboutcomponent.ts

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  public productsdetails: any;
+  public getproductdetail(product: any) {
+    console.log(product)
+    this.productsdetails = product;
 
+  }
+  public products = [
+    {
+      id: 1,
+      Name: "product1",
+      price: 100,
+      description: "dnsmk"
+    },
+    {
+      id: 2,
+      Name: 'product2',
+      price: 200,
+      description: "dnsmk"
+    },
+    {
+      id: 3,
+      Name: 'product3',
+      price: 300,
+      description: "dnsmk"
+    },
+    {
+      id: 5,
+      Name: 'product5',
+      price: 500,
+      description: "dnsmk"
+    },
+    {
+      id: 6,
+      Name: 'product6',
+      price: 600,
+      description: "dnsmk"
+    }
+  ]
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,9 +12,9 @@ public con=true;
 public data1 = true;
 public data2 = [
   {name: 'nnnn',
-id:3},{name: 'nnnn',
-id:3},{name: 'nnnn',
-id:3}
+id:1},{name: 'product1',price:100,
+id:2},{name: 'product2',price:200,
+id:3},{name:'product3',price:300}
 ]
 
 
@@ -29,3 +29,5 @@ public  fun2() {
   alert('fun2');
 }
 }
+
+
